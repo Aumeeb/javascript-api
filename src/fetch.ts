cfcdc29df148560bff3b9f2d1e5c08d7edd82419
noberk <421471260@qq.com>
2018-01-21 21:37:20 +0800

    架构变动

--- src/fetch.ts
+++
@@ -1,9 +0,0 @@
-export class Http {
-    static get(path: string): Promise<Response> {
-        return fetch(path)
-    }
-}
-
-export  class DataUrl {
-    static readonly getJsSysAPI = './data/js.json';
-}
