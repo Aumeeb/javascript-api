2f2e16f76ff8d391d4b4b89382a69ca60cb3303c
noberk <421471260@qq.com>
2018-02-09 18:41:08 +0800

    设置好redux布局

---
+++ src/redux/homeRedux.ts
@@ -0,0 +1,40 @@
+import { JsSysAPI } from "../routes/home/homeTypes";
+import { AnyAction } from "redux";
+import { infiniteTake, DataUrl, take } from "../data/fetch";
+ 
+type FetchAction<T> = { type: any, data: Promise<T> };
+
+export type HomeActionType = "getSysAPIName" | "getSysKeywordName" | "getNewFeaturesName" | undefined;
+
+export class HomeAction implements AnyAction {
+    [extraProps: string]: any;
+    type: HomeActionType;
+
+    static getSysAPIName<T>(): FetchAction<T> {
+        return {
+            type: 'getSysAPIName',
+            data: take<T>(DataUrl.JsSysAPIAddress)
+        }
+    }
+    static getSysKeywordName<T>(): FetchAction<T> {
+        return {
+            type: 'getSysKeywordName',
+            data: take<T>(DataUrl.keywordsAddress)
+        }
+    }
+}
+
+
+
+
+
+export  function homeReducer(state = 1, actionType: HomeAction) {
+    switch (actionType.type) {
+        case 'getSysAPIName':
+            return state;
+        case 'getSysKeywordName':
+            return state;
+        default:
+            return state
+    }
+}
